4558b821401344d7f2cfea0dc6dcecd3eb9eec0a
Beneetta Sabu <beneettasabu05@gmail.com>
2025-06-12 14:41:29 +1000

    Fix: add BrowserRouter import

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Make sure this is imported
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-</StrictMode>
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
